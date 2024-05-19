261880b4e06b980cfac85f3bba11ad2304058361
narinn-star <skfls2618@naver.com>
2024-05-20 01:02:23 +0900

    Style : Signin Page UI
    - background 잘림 현상 해결
    - ButtonXL Component 추가

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,13 +1,16 @@
+import Image from 'next/image';
+import IllustSignIn from '/public/images/Illust_signIn.png';
+import ButtonXL from '@/components/buttons/ButtonXL';
+
 export default function Signin() {
   return (
-    <>
-      <div className="absolute top-0 left-0 bg-main_bg h-full">
-        {/* <Image src={IllustSignIn} alt="signin" /> */}
-        <div className="mt-[50px] flex justify-center">
-          {/* <GoogleSignInButton></GoogleSignInButton> */}
-          signin
+    <div className="w-screen h-screen bg-cover bg-fixed bg-background">
+      <div className="absolute top-0 left-0 bg-background">
+        <Image src={IllustSignIn} alt="signin" />
+        <div className="mt-[50px] mb-[40px] flex justify-center bg-background">
+          <ButtonXL>구글로 로그인하기</ButtonXL>
         </div>
       </div>
-    </>
+    </div>
   );
 }
