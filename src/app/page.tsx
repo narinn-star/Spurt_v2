ce57dd355a776d70851374b1ac40d3aa9de2f913
narinn-star <skfls2618@naver.com>
2024-05-26 02:55:39 +0900

    Refactor : Container CSS
    - MainContainer 추가
    - ContentContainer 추가
    - globalCSS
      - media query 추가
        - max-w-responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,47 @@
+'use client';
+
 import ButtonL from '@/components/buttons/ButtonL';
+import ContentsContainer from '@/components/containers/ContentContainer';
+import MainContainer from '@/components/containers/MainContainer';
 import Header from '@/components/header/Header';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="bg-main-100 flex flex-col items-center">
-      <Header />
-      <div className="flex flex-row w-[1000px] justify-between">
-        <div className="flex flex-col  mt-[60px] text-title1 mb-[12px]">
-          <p className="underline underline-offset-8 decoration-main-400 decoration-4">
-            꽁지님 안녕하세요
-          </p>
-          <p>오늘도 마지막까지 화이팅!</p>
+    <>
+      <MainContainer>
+        <Header />
+        <div className="flex flex-row justify-between max-w-responsive">
+          <div className="flex flex-col mt-[60px] text-title1 mb-[12px]">
+            <p className="underline underline-offset-8 decoration-main-400 decoration-4">
+              꽁지님 안녕하세요
+            </p>
+            <p>오늘도 마지막까지 화이팅!</p>
+          </div>
+          <div className="flex items-end mb-[12px]">
+            <Link href={'/post'}>
+              <ButtonL>질문-답변 만들기</ButtonL>
+            </Link>
+          </div>
         </div>
-        <div className="flex items-end mb-[12px]">
-          <Link href={'/post'}>
-            <ButtonL>질문-답변 만들기</ButtonL>
-          </Link>
-        </div>
-      </div>
+        <Image
+          priority
+          src="/images/Illust_question.png"
+          width="1000"
+          height="320"
+          alt="questionIllust"
+          className="min-w-[1000px]"
+        />
+      </MainContainer>
 
-      <Image
-        priority
-        src="/images/Illust_question.png"
-        width="1000"
-        height="320"
-        alt="questionIllust"
-        className="min-w-[1000px]"
-      />
-    </div>
+      <ContentsContainer title="나의 질문 모음">
+        <div></div>
+      </ContentsContainer>
+
+      <ContentsContainer title={'사람들이'} subtitle="최근에 올린 질문">
+        <div></div>
+      </ContentsContainer>
+    </>
   );
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -35,62 +35,61 @@ const Header = () => {
 
   return (
     <>
-      <div className="flex flex-row h-[100px] justify-center">
-        <div className="flex justify-between items-center w-[1000px]">
-          <div className="flex items-center">
-            <button onClick={() => router.push('/')}>
-              <Image
-                priority
-                src="/icons/logo.svg"
-                alt="logo"
-                width={124}
-                height={40}
-                style={{ width: 124, height: 40 }}
-              />
-            </button>
-            <div className="flex justify-between ml-[130px]">
-              <Link
-                href="/"
-                className={`${
-                  pathName === '/' ? 'text-heading1' : 'text-heading4'
-                } mr-[50px]`}
-              >
-                질문 모음
-              </Link>
-              <Link
-                href="/experience"
-                className={`${
-                  pathName === '/experience' ? 'text-heading1' : 'text-heading4'
-                } mr-[50px]`}
-              >
-                나의 경험
-              </Link>
-              <Link
-                href="/note"
-                className={`${
-                  pathName === '/note' ? 'text-heading1' : 'text-heading4'
-                } mr-[50px]`}
-              >
-                요약 노트
-              </Link>
-            </div>
+      <div className="flex h-[100px] justify-between items-center max-w-responsive">
+        <div className="flex items-center">
+          <button onClick={() => router.push('/')}>
+            <Image
+              priority
+              src="/icons/logo.svg"
+              alt="logo"
+              width={124}
+              height={40}
+              style={{ width: 124, height: 40 }}
+            />
+          </button>
+          <div className="flex ml-[130px]">
+            <Link
+              href="/"
+              className={`${
+                pathName === '/' ? 'text-heading1' : 'text-heading4'
+              } mr-[50px]`}
+            >
+              질문 모음
+            </Link>
+            <Link
+              href="/experience"
+              className={`${
+                pathName === '/experience' ? 'text-heading1' : 'text-heading4'
+              } mr-[50px]`}
+            >
+              나의 경험
+            </Link>
+            <Link
+              href="/note"
+              className={`${
+                pathName === '/note' ? 'text-heading1' : 'text-heading4'
+              } mr-[50px]`}
+            >
+              요약 노트
+            </Link>
           </div>
-          <div className="" ref={profileRef}>
-            <button onClick={clickProfile}>
-              <Image
-                priority
-                src="/icons/pin-fill-42.svg"
-                width="50"
-                height="50"
-                className="rounded-full"
-                alt="프로필 사진"
-              ></Image>
-            </button>
-            {isProfileOpened && (
-              <ProfileModal handleModal={clickProfile}></ProfileModal>
-            )}
+        </div>
+        <div className="" ref={profileRef}>
+          <button onClick={clickProfile}>
+            <Image
+              priority
+              src="/icons/pin-fill-42.svg"
+              width="50"
+              height="50"
+              className="rounded-full"
+              alt="프로필 사진"
+            ></Image>
+          </button>
+          {isProfileOpened && (
+            <ProfileModal handleModal={clickProfile}></ProfileModal>
+          )}
 
-            {/* {user ? (
+          {/* {user ? (
             <>
               <button onClick={clickProfile}>
                 <Image
@@ -108,7 +107,6 @@ const Header = () => {
               <LoginButton>로그인</LoginButton>
             </Link> 
           )} */}
-          </div>
         </div>
       </div>
     </>
