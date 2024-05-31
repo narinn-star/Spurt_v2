872a3167b274e7962c7cac62d0f6bdd682ac734c
narinn-star <skfls2618@naver.com>
2024-06-01 01:08:21 +0900

    Style : SummaryNote UI Completed(#12)
    - Keyword Component 추가
    - Container 분리 필요

--- src/app/summaryNote/page.tsx
+++ src/app/summaryNote/page.tsx
@@ -1,9 +1,11 @@
 'use client';
 import ButtonL from '@/components/buttons/ButtonL';
+import ButtonS from '@/components/buttons/ButtonS';
 import SummaryCard from '@/components/cards/SummaryCard';
 import ContentsContainer from '@/components/containers/ContentContainer';
 import MainContainer from '@/components/containers/MainContainer';
 import Header from '@/components/header/Header';
+import Keyword from '@/components/keywords/Keyword';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -44,6 +46,57 @@ export default function SummaryNote() {
         <div className="flex gap-3">
           <SummaryCard title="1번" isPinned={true} bgColor="bg-main-200" />
           <SummaryCard title="2번" isPinned={false} bgColor="bg-main-200" />
+        </div>
+
+        <div className="flex flex-row mt-[30px] mb-[50px]">
+          <div className="items-center max-w-[390px] w-full">
+            <div className="flex flex-row justify-between mb-[20px] ">
+              <p className="text-heading1">답변 키워드</p>
+              <Image
+                priority
+                src="/icons/eye-on-40.svg"
+                alt="eyeOnIcons"
+                width={40}
+                height={40}
+              />
+            </div>
+            <Keyword
+              text="마크다운과 직접 입력으로 키워드를 등록할 수 있어요"
+              isVisible={true}
+            />
+            <Keyword
+              text="면접을 준비하는 취업준비생을 위한 서비스인데용, SPURT"
+              isVisible={true}
+            />
+          </div>
+          <div className="border-[0.7px] mx-[30px] bg-[#989898]" />
+          <div className="items-center max-w-[490px] w-full">
+            <div className="flex flex-row justify-between items-center max-w-[490px] w-full mb-[20px]">
+              <p className="text-heading1">전체 답변</p>
+              <Image
+                priority
+                src="/icons/eye-off-40.svg"
+                alt="eyeOffIcons"
+                width={40}
+                height={40}
+              />
+            </div>
+            <div className="text-gray-600 whitespace-pre-line blur-[3px]">
+              전체 답변 내용
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-end">
+          <ButtonS option="black">
+            수정하기
+            <Image
+              src="/icons/edit-16.svg"
+              alt="editIcon"
+              width={16}
+              height={16}
+              priority
+            />
+          </ButtonS>
         </div>
       </ContentsContainer>
     </>
