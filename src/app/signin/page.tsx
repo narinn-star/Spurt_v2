b73895440d7927dedf6b8dbdba31b3e9cb5790aa
narinn-star <skfls2618@naver.com>
2024-07-10 16:45:15 +0900

    Feat : OAuth Google ..ing(#19)

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,15 +1,23 @@
 import Image from 'next/image';
 import IllustSignIn from '/public/images/Illust_signIn.png';
 import ButtonXL from '@/components/buttons/ButtonXL';
+import { signIn } from '@/auth';
 
 export default function Signin() {
   return (
     <div className="w-screen h-screen bg-cover bg-fixed bg-background">
       <div className="absolute bg-background">
         <Image src={IllustSignIn} alt="signin" />
-        <div className="mt-[50px] mb-[40px] flex justify-center bg-background">
-          <ButtonXL>구글로 로그인하기</ButtonXL>
-        </div>
+        <form
+          action={async () => {
+            'use server';
+            await signIn('google');
+          }}
+        >
+          <div className="mt-[50px] mb-[40px] flex justify-center bg-background">
+            <ButtonXL>구글로 로그인하기</ButtonXL>
+          </div>
+        </form>
       </div>
     </div>
   );
