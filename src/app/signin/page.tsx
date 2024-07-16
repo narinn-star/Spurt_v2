d28a82b02698ef23490b93a7998e247348f3d3b6
narinn-star <skfls2618@naver.com>
2024-07-17 03:31:42 +0900

    Feat: Google OAuth Cookie Test
    - cookie 값 넣어지는 지 테스트중

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,23 +1,23 @@
+'use client';
 import Image from 'next/image';
 import IllustSignIn from '/public/images/Illust_signIn.png';
 import ButtonXL from '@/components/buttons/ButtonXL';
-import { signIn } from '@/auth';
+import client from '../api/client';
 
 export default function Signin() {
+  const handleSignIn = async () => {
+    console.log('clicked');
+    //client.get(`/oauth2/authorization/google`);
+    window.location.href =
+      'https://alpha.spurtapp.com/oauth2/authorization/google';
+  };
   return (
     <div className="w-screen h-screen bg-cover bg-fixed bg-background">
       <div className="absolute bg-background">
-        <Image src={IllustSignIn} alt="signin" />
-        <form
-          action={async () => {
-            'use server';
-            await signIn('google');
-          }}
-        >
-          <div className="mt-[50px] mb-[40px] flex justify-center bg-background">
-            <ButtonXL>구글로 로그인하기</ButtonXL>
-          </div>
-        </form>
+        <Image src={IllustSignIn} alt="signin" priority />
+        <div className="mt-[50px] mb-[40px] flex justify-center bg-background">
+          <ButtonXL onClick={handleSignIn}>구글로 로그인하기</ButtonXL>
+        </div>
       </div>
     </div>
   );
--- src/components/buttons/ButtonXL.tsx
+++ src/components/buttons/ButtonXL.tsx
@@ -2,11 +2,15 @@ import { ButtonHTMLAttributes } from 'react';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: '';
+  onClick?: () => {};
 }
 
 const ButtonXL = (props: ButtonProps) => {
   return (
-    <button className="h-16 w-[500px] text-white text-heading1 rounded-[50px] bg-gray-700">
+    <button
+      onClick={props.onClick}
+      className="h-16 w-[500px] text-white text-heading1 rounded-[50px] bg-gray-700"
+    >
       {props.children}
     </button>
   );
